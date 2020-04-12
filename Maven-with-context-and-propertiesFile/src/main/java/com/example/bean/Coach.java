cd0c92b97a34b904364799b34b10e81ef8772d19
Bhanu <Bhanu@Bhanu>
2020-04-12 23:01:04 +0530

    Commit

---
+++ Maven-with-context-and-propertiesFile/src/main/java/com/example/bean/Coach.java
@@ -0,0 +1,34 @@
+package com.example.bean;
+
+public class Coach {
+	
+	private String eamil;
+	private String team;
+	public String getEamil() {
+		return eamil;
+	}
+	public void setEamil(String eamil) {
+		this.eamil = eamil;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	@Override
+	public String toString() {
+		return "Coach [eamil=" + eamil + ", team=" + team + "]";
+	}
+	
+	
+public void initializeBean()
+{
+	System.out.println("bean initialized..");
+}
+
+public void finalizedBean()
+{
+	System.out.println("bean is going to destroy");
+}
+}
