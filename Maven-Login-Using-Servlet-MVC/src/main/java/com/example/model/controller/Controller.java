983c1a2f735df973ec93918703632efb93a69ba6
Bhanu <Bhanu@Bhanu>
2020-04-18 14:56:45 +0530

    Commit

---
+++ Maven-Login-Using-Servlet-MVC/src/main/java/com/example/model/controller/Controller.java
@@ -0,0 +1,52 @@
+package com.example.model.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProcess(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProcess(request, response);
+	}
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String userName=request.getParameter("u_name").toString();
+		String password=request.getParameter("u_pass").toString();
+		if((userName.equalsIgnoreCase("Bhanu") &&(password.equalsIgnoreCase("kiran"))))
+		{
+			RequestDispatcher view=request.getRequestDispatcher("success.html");
+			view.forward(request, response);
+		}
+		else
+		{
+			RequestDispatcher view=request.getRequestDispatcher("error.html");
+			view.forward(request, response);
+		}
+		
+	}
+}
+
+
