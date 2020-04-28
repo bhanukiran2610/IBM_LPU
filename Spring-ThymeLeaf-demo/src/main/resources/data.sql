444f5e241bc6a7ff066fa15e9bc1aa4bfee0e272
Bhanu <Bhanu@Bhanu>
2020-04-28 10:25:01 +0530

    Commit Thymeleaf

---
+++ Spring-ThymeLeaf-demo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into products(product_name,product_desc,product_price) values('Iphone SE','Apples  product',300);
+insert into products(product_name,product_desc,product_price) values('Rolex watch','A watch',500);
+insert into products(product_name,product_desc,product_price) values('One plus 8 pro','Flag ship mobile',100);
