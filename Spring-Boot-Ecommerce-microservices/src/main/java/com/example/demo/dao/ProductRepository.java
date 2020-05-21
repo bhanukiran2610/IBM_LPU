cf7f42ebbba0b7edd56ff12ef0bf1fe9bcef2bb8
bhanukiran2610 <bhanukiran2610.c@gmail.com>
2020-05-21 10:59:42 +0530

    Angular Files Required

--- Spring-Boot-Ecommerce-microservices/src/main/java/com/example/demo/dao/ProductRepository.java
+++ Spring-Boot-Ecommerce-microservices/src/main/java/com/example/demo/dao/ProductRepository.java
@@ -1,10 +1,23 @@
 package com.example.demo.dao;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
+
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	@Query
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+	
+	@Query
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
